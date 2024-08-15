7a68708a95fd7bb8bea7ace476fdbba1e2982271
Shadowstep1720 <74839190+Shadowstep1720@users.noreply.github.com>
2024-08-15 16:01:40 -0500

    Create file structure, add basic functions

---
+++ scripts/calculator.js
@@ -0,0 +1,8 @@
+// Basic math functions
+const add = (addend1, addend2) => addend1 + addend2;
+const subtract = (minuend, subtrahend) => minuend - subtrahend;
+const multiply = (multiplend1, multiplend2) => multiplend1 * multiplend2;
+const divide = (numerator, denominator) => numerator / denominator;
+const power = (base, exponent) => base ** exponent;
+
+// accepts parsed input andd returns calculation
