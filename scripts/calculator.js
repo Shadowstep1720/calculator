ae2fbf57e2615b092e966f1b1670e43f14d2c5e2
Shadowstep1720 <74839190+Shadowstep1720@users.noreply.github.com>
2024-08-16 13:15:45 -0500

    Add event listeners and display updates

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,12 +1,12 @@
 // Basic math functions
-const add = (addend1, addend2) => addend1 + addend2;
-const subtract = (minuend, subtrahend) => minuend - subtrahend;
-const multiply = (multiplend1, multiplend2) => multiplend1 * multiplend2;
-const divide = (numerator, denominator) => numerator / denominator;
-const power = (base, exponent) => base ** exponent;
+function add(addend1, addend2) { addend1 + addend2 };
+function subtract(minuend, subtrahend) { minuend - subtrahend };
+function multiply(multiplend1, multiplend2) { multiplend1 * multiplend2 };
+function divide(numerator, denominator) { numerator / denominator }
+function power(base, exponent) { base ** exponent }
 
-// accepts parsed input and returns calculation
-const operate = (number1, number2, operation) => {
+// Accepts parsed input and returns calculation
+function operate(number1, number2, operation) {
     let result;
     switch (operation) {
         case "+":
@@ -26,6 +26,40 @@ const operate = (number1, number2, operation) => {
             break;
     }
     return result;
+}
+
+// Event listener functions
+
+// Button event listeners
+const inputButtons = document.querySelectorAll(".input");
+for (button of inputButtons) {
+    button.addEventListener("click", (event) => {
+        let newText = event.target.textContent;
+        updateDisplay(newText);
+    })
+}
+
+// Update and clear display
+function updateDisplay(newText) {
+    const display = document.querySelector("#display");
+    let operators = "+-^x/";
+    if (newText === "Clear") {
+        display.textContent = "";
+    } else if (newText === ".") {
+        decimalDisabled(true);
+        display.textContent += newText;
+    } else if (operators.includes(newText)) {
+        decimalDisabled(false);
+        display.textContent += ` ${newText} `;
+    } else {
+        display.textContent += newText;
+    }
+}
+
+// Disable and re-enable decimal button
+function decimalDisabled(bool) {
+    const decimalBtn = document.querySelector("#decimal");
+    decimalBtn.disabled = bool
 }
 
 
