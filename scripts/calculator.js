18c11100c617276796e73caa5887338e78466f67
Shadowstep1720 <74839190+Shadowstep1720@users.noreply.github.com>
2024-08-16 15:11:54 -0500

    remove console.log statements

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -7,47 +7,25 @@ const power = (base, exponent) => base ** exponent;
 
 // Accepts parsed input and returns calculation
 function operate(inputArray) {
-    console.log("operate called with:");
-    console.log(inputArray)
-    let resultArray, number1, number2, operation;
+    let number1, number2, operation;
     if (inputArray[0]) {
         number1 = parseFloat(inputArray[0]);
-        console.log(`number one assigned as: `)
-        console.log(number1);
     } else {
-        console.log("0 returned d/t inputArray[0] not found")
         return [0];
     }
 
     if (inputArray[1]) {
         operation = inputArray[1];
-        console.log(`operation assigned as:`);
-        console.log(operation)
     } else {
-        console.log("number1 returned d/t inputArray[1] not found")
         return [number1];
     }
 
     if (inputArray[2]) {
         number2 = parseFloat(inputArray[2]);
-        console.log(`number2 assigned as: `)
-        console.log(number2)
     } else {
-        console.log("number1 returned d/t inputArray[2] not found")
         return [number1];
     }
-    inputArray
-    console.log("array before shift")
-    console.log(inputArray)
-    inputArray.shift()
-    console.log("after 1 shift")
-    console.log(inputArray)
-    inputArray.shift()
-    console.log("after 2 shifts")
-    console.log(inputArray)
-    inputArray.shift()
-    console.log("after 3 shifts")
-    console.log(inputArray)
+    inputArray.splice(0, 3);
     switch (operation) {
         case "+":
             inputArray.unshift(add(number1, number2));
@@ -65,8 +43,6 @@ function operate(inputArray) {
             inputArray.unshift(power(number1, number2));
             break;
     }
-    console.log("result of operation unshifted to beginning of result array. Full array now:")
-    console.log(inputArray)
     return inputArray;
 }
 
@@ -109,18 +85,12 @@ function decimalDisabled(bool) {
 }
 
 function parseInput(input) {
-    console.log("parseInput called, original input")
-    console.log(input)
     let inputArray = input.split(' ');
-
-    console.log("array after split")
-    console.log(inputArray);
     return inputArray;
 }
 
 // "=" click event handler
 function enterHandler() {
-    console.log("enter handler called")
     let display = document.querySelector("#display");
     let input = display.textContent;
     let parsedInput = parseInput(input);
