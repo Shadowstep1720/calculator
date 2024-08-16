131acddb741a29250798899d9e32fc4420747015
Shadowstep1720 <74839190+Shadowstep1720@users.noreply.github.com>
2024-08-16 12:28:30 -0500

    Create calculator display and operate function

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -5,4 +5,28 @@ const multiply = (multiplend1, multiplend2) => multiplend1 * multiplend2;
 const divide = (numerator, denominator) => numerator / denominator;
 const power = (base, exponent) => base ** exponent;
 
-// accepts parsed input andd returns calculation
+// accepts parsed input and returns calculation
+const operate = (number1, number2, operation) => {
+    let result;
+    switch (operation) {
+        case "+":
+            result = add(number1, number2);
+            break;
+        case "-":
+            result = subtract(number1, number2);
+            break;
+        case "x":
+            result = multiply(number1, number2);
+            break;
+        case "/":
+            result = divide(number1, number2);
+            break;
+        case "^":
+            result = power(number1, number2);
+            break;
+    }
+    return result;
+}
+
+
+
